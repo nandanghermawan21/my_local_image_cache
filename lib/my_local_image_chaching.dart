034aa83de59b10bc4d24e0930e55464a6674b9ac
nandanghermawan <nandang@enerren.com>
2024-04-01 10:32:41 +0700

    first commit

---
+++ lib/my_local_image_chaching.dart
@@ -0,0 +1,71 @@
+library my_local_image_chaching;
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+class LocalImageCache extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final double height;
+  final double width;
+  final BoxDecoration? decoration;
+
+  const LocalImageCache({
+    super.key,
+    required this.name,
+    this.height = 100,
+    this.width = 100,
+    this.imageUrl =
+        "https://enerren.com/wp-content/uploads/2016/04/logo-enerren.png",
+    this.decoration,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: width,
+      decoration: decoration,
+      child: FutureBuilder(
+        future: saveAndLoadImage(imageUrl, name),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Image.file(
+              snapshot.data!,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) => const Icon(
+                Icons.error,
+                color: Colors.grey,
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  //buat function untuk mengecek apakah gambar sudah ada di dalam file
+  //jika sudah ada maka tampilkan gambar tersebut
+  //jika belum ada maka download gambar tersebut dan simpan ke dalam file
+  //lalu tampilkan gambar tersebut
+  Future<File> saveAndLoadImage(String imageUrl, String name) async {
+    final response = await http.get(Uri.parse(imageUrl));
+    final bytes = response.bodyBytes;
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/$name');
+    await file.writeAsBytes(bytes);
+    return file;
+  }
+}
